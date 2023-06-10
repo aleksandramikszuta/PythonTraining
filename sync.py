0d0ed3322b0d3de4c79520c787cf8bb5776ea86b
Aleksandra Mikszuta <ola.mikszuta@gmail.com>
2023-06-10 21:14:24 +0200

    Logging copying and removing

--- sync.py
+++ sync.py
@@ -5,29 +5,34 @@ from os import remove
 from shutil import copytree, copy, rmtree
 import os
 
-def deep_sync(source, target):
+def log(text, logfile):
+    print(text)
+    logfile.write(text + "\n")
+
+def deep_sync(source, target, logfile):
     result = dircmp(source, target)
     result.compare = cmp
 
     for toCopy in result.diff_files:
-        #print ("COPYING " + os.path.join(source, toCopy) + " " + target)
+        log("COPYING " + os.path.join(source, toCopy) + " to " + target, logfile)
         copy(os.path.join(source, toCopy), target)
 
     for toCopy in result.left_only:
-        #print ("COPYING " + os.path.join(source, toCopy) + " " + target)
+        log("COPYING " + os.path.join(source, toCopy) + " to " + target, logfile)
         if os.path.isdir(os.path.join(source, toCopy)):
             copytree(os.path.join(source, toCopy), os.path.join(target, toCopy))
         else:
             copy(os.path.join(source, toCopy), target)
     
     for toRemove in result.right_only:
+        log("REMOVING " + os.path.join(target, toRemove), logfile)
         if os.path.isdir(os.path.join(target, toRemove)):
             rmtree(os.path.join(target, toRemove))
         else:
             remove(os.path.join(target, toRemove))
 
     for subdir in result.subdirs:
-        deep_sync(os.path.join(source, subdir), os.path.join(target, subdir))
+        deep_sync(os.path.join(source, subdir), os.path.join(target, subdir), logfile)
         
 
 def main():
@@ -36,12 +41,15 @@ def main():
                   help="synchronisation interval (in minutes)", default="1")
     parser.add_option("-s", "--source", dest="source", help="source folder - MANDATORY")
     parser.add_option("-t", "--target", dest="target", help="target folder - MANDATORY")
+    parser.add_option("-l", "--logfile", dest="logfile", help="Path to log file", default="log.txt")
     (options, args) = parser.parse_args()
     if (options.source is None or options.target is None):
         parser.print_help()
         exit()
 
-    deep_sync(options.source, options.target)
+    logfile = open(options.logfile, "w")
+    deep_sync(options.source, options.target, logfile)
+    logfile.close()
 
 if __name__=="__main__":
     main()
